981a00b7708b98f6f101121f014a1a94c5fd6773
Farah Elhasan <farahelhasan32@gmail.com>
2025-04-06 22:22:10 +0300

    Create SnowBot class

---
+++ Bots/SnowBot.cs
@@ -0,0 +1,30 @@
+﻿using WeatherMonitoringApp.Models;
+
+namespace WeatherMonitoringApp.Bots
+{
+    class SnowBot : IBot
+    {
+        private readonly string _message;
+
+        private readonly double _temperatureThreshold;
+        public string BotName => "SnowBot";
+        public bool IsEnabled { get; }
+        public SnowBot(BotConfiguration config)
+        {
+            IsEnabled = config.Enabled;
+            _temperatureThreshold = config.TemperatureThreshold;
+            _message = config.Message;
+        }
+
+        public void Notify(WeatherData data)
+        {
+            if (!IsEnabled) return;
+
+            if (data.Temperature <= _temperatureThreshold)
+            {
+                Console.WriteLine($"{BotName} activated!");
+                Console.WriteLine($"{BotName}: \"{_message}\"");
+            }
+        }
+    }
+}
