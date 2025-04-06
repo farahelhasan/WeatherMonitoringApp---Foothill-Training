1891f611d0ec0afc15323f400bd173b909e62973
Farah Elhasan <farahelhasan32@gmail.com>
2025-04-06 20:29:39 +0300

    Create JsonWeatherParser class

---
+++ Parsers/JsonWeatherParser.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Text.Json;
+using WeatherMonitoringApp.Models;
+
+namespace WeatherMonitoringApp.Parsers
+{
+    class JsonWeatherParser : IWeatherDataParser
+    {
+        public WeatherData Parse(string input)
+        {
+            var jsonDoc = JsonDocument.Parse(input);
+            var root = jsonDoc.RootElement;
+
+            var location = root.GetProperty("Location").GetString();
+            var temperature = root.GetProperty("Temperature").GetDouble();
+            var humidity = root.GetProperty("Humidity").GetDouble();
+
+            return new WeatherData(location, temperature, humidity);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using WeatherMonitoringApp.Models;
+using WeatherMonitoringApp.Parsers;
+
 Console.WriteLine("Hello, World!");
+JsonWeatherParser x = new JsonWeatherParser();
+string json = @"
+            {
+                ""Location"": ""New York"",
+                ""Temperature"": 22.5,
+                ""Humidity"": 60.0
+            }";
+WeatherData c = x.Parse(json);
+Console.WriteLine(c);
