7332cc2fb6c6a975e8514dfbd8cccdaeb7ea240c
Farah Elhasan <farahelhasan32@gmail.com>
2025-04-06 22:06:23 +0300

    Create BotConfiguration class

---
+++ Bots/BotConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherMonitoringApp.Bots
+{
+    class BotConfiguration
+    {
+        public bool Enabled { get; set; }
+        public string Message { get; set; }
+        public double TemperatureThreshold { get; set; }
+        public double HumidityThreshold { get; set; }
+    }
+}
