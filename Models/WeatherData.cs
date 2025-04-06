5504173c18f8e27a5623b635811687a44dfb392e
Farah Elhasan <farahelhasan32@gmail.com>
2025-04-06 19:25:13 +0300

    Create WeatherData model

---
+++ Models/WeatherData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringApp.Models
+{
+    class WeatherData
+    {
+        public string Location { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+
+        public WeatherData(string location, double temperature, double humidity)
+        {
+            Location = location;
+            Temperature = temperature;
+            Humidity = humidity;
+        }
+
+        public override string ToString()
+        {
+            return $"Location: {Location}, Temperature: {Temperature}, Humidity: {Humidity}";
+        }
+    }
+}
