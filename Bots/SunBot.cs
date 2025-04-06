e24326dd9f5c56c5846def6b74e80c15c3ba10be
Farah Elhasan <farahelhasan32@gmail.com>
2025-04-06 22:20:05 +0300

    Create SunBot class

---
+++ Bots/SunBot.cs
@@ -0,0 +1,30 @@
+﻿using WeatherMonitoringApp.Models;
+
+namespace WeatherMonitoringApp.Bots
+{
+    class SunBot : IBot
+    {
+        private string _message;
+
+        private double _temperatureThreshold;
+        public string BotName => "SunBot";
+        public bool IsEnabled { get; }
+        public SunBot(BotConfiguration config)
+        {
+            IsEnabled = config.Enabled;
+            _temperatureThreshold = config.TemperatureThreshold;
+            _message = config.Message;
+        }
+        public void Notify(WeatherData data)
+        {
+            if (!IsEnabled) return;
+
+            if (data.Temperature >= _temperatureThreshold)
+            {
+                Console.WriteLine($"{BotName} activated!");
+                Console.WriteLine($"{BotName}: \"{_message}\"");
+            }
+        }
+       
+    }
+}
