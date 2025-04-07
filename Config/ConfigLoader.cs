179cde4617de4908fb334220bdd9455c2347393f
Farah Elhasan <farahelhasan32@gmail.com>
2025-04-07 12:18:29 +0300

    Create ConfigLoader to convert config.json to obj to use in code

---
+++ Config/ConfigLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace WeatherMonitoringApp.Config
+{
+    class ConfigLoader
+    {
+        public static BotSettings Load(string path)
+        {
+            var json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<BotSettings>(json);
+        }
+    }
+}
