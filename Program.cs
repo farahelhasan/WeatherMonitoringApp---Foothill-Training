2d955f31b36ee475ee4218e5266d98e7cc5f57d6
Farah Elhasan <farahelhasan32@gmail.com>
2025-04-07 13:11:27 +0300

    Store all the bots in a List<IBot> in Progrem.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WeatherMonitoringApp.Bots;
+using WeatherMonitoringApp.Config;
+
+namespace WeatherMonitoringApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+                var config = ConfigLoader.Load("config.json");
+
+                // Create bots
+                List<IBot> bots = new()
+                {
+                    new RainBot(config.RainBot),
+                    new SunBot(config.SunBot),
+                    new SnowBot(config.SnowBot)
+                };
+        
+        }
+        }
+
+    }
+
